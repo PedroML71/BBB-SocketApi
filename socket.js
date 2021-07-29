5c593f7da4f68b5e027f2a39373860aec9bb1ec2
brenno-calado <brenno.calado@gmail.com>
2021-07-29 00:15:47 -0300

    remove cors origin

--- socket.js
+++ socket.js
@@ -4,12 +4,7 @@ let io;
 module.exports = {
   // funcao que inicia o socket
   init: (httpServer) => {
-    io = require("socket.io")(httpServer, {
-      cors: {
-        origin: "https://bbbdev.artstech.com.br/",
-        methods: ["GET", "POST"],
-      },
-    });
+    io = require("socket.io")(httpServer);
 
     return io;
   },

3db544f351cda8f1321cf9bf5858db667b5590a3
brenno-calado <brenno.calado@gmail.com>
2021-07-30 10:46:23 -0300

    add jest, user/status/warning test

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,23 @@
-const frisby = require('firsby');
+const frisby = require('frisby');
 
-const HEROKU_URL = 'https://bbb-heroku-test.herokuapp.com/';
+const HEROKU_URL = 'https://bbb-heroku-test.herokuapp.com';
 
-describe('')
+describe('rota /user/status/warning', () => {
+  it('retorna "sinal warning enviado"', async () => {
+    await frisby
+      .post(`${HEROKU_URL}/user/status/warning`,
+        {
+          userId: 'uma string qualquer',
+        })
+      .expect('status', 200)
+      .then((response) => {
+        const { body } = response;
+        const result = JSON.parse(body);
+        expect(result.message).toBe('sinal warning enviado');
+      });
+  });
+});
+
+describe('rota /test/connection', () => {});
+
+describe('rota /test/error', () => {});
